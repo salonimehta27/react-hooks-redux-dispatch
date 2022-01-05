170e47a73525a2fa87a3a497a9f567f5c1245444
Saloni <salonimehta2127@gmail.com>
2022-01-05 00:23:51 -0500

    Completed

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+let state = { count: 0 };
+let action = { type: "counter/increment" };
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +9,19 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function dispatch(action){
+ state=changeState(state,action);
+ render()
+}
+
+function render(){
+  document.body.textContent=state.count;
+}
+render();
+dispatch(action)
+dispatch(action)
+dispatch(action)
+dispatch(action)
+
 
 changeState(state, action);
